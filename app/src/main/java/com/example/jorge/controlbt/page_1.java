f05006a17d2b9bd8013a1ce2ffd68a7c858b1741
unknown <jorgealopez@unicesar.edu.co>
2018-12-01 11:21:56 -0500

    ElectrickApp-v0

---
+++ app/src/main/java/com/example/jorge/controlbt/page_1.java
@@ -0,0 +1,23 @@
+package com.example.jorge.controlbt;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class page_1 extends Fragment {
+
+    //Constructor default
+    public page_1(){};
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View PageOne = inflater.inflate(R.layout.page1, container, false);
+
+
+
+        return PageOne;
+    }
+}
---
+++ app/src/main/java/com/example/jorge/controlbt/page_3.java
@@ -0,0 +1,20 @@
+package com.example.jorge.controlbt;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class page_3  extends Fragment {
+
+    //Constructor default
+    public page_3(){};
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View PageTree = inflater.inflate(R.layout.page3, container, false);
+
+        return PageTree;
+    }
+}
